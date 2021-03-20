09dc24f064f4208eb5ad61e49ab798ce67145ed3
uronohan <uronohan@gmail.com>
2021-03-20 22:39:30 +0700

    checkpoint

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -16,45 +16,44 @@ class CarForm extends Component{
                 [e.target.name]: e.target.value
             })
         }
+
+        // handleSubmit = (e) => {
+        //     this.setState({
+                
+        //     })
+        // }
     
         render() {
             return (
-                <div className=''>
-                    Car Management System
-                    <div className=''>
-                        <div className=''>
-                            <div className=''>
-                                <div className='car-plate-center'>
-                                    <label className='inner-cms-center'>
-                                        License Plate
-                                    <input type='text' value={this.state.licensePlate} onChange={this.handleChange} name='licensePlate' />
-                                    </label>
-                                </div>
-                                <div className='car-brands-center'> 
-                                    <label className='inner-cms-center'>
-                                        Brands
-                                    <input type='text' value={this.state.carBrands} onChange={this.handleChange} name='carBrands'/>    
-                                </label>
-                                </div>
-                                <div className='car-models-center'> 
-                                    <label className='inner-cms-center'>
-                                        Models
-                                    <input type='text' value={this.state.carModels} onChange={this.handleChange} name='carModels'/>
-                                    </label>
-                                </div>
-                                <div className='car-remarks-center'>
-                                    <label className='inner-cms-center'>
-                                        Remark
-                                        <input type='text' value={this.state.carRemarks} onChange={this.handleChange} name='carRemarks'/>
-                                    </label>
-                                </div>
-                                <div className='cms-button'>
-                                    <button type='button' className='cms-save-button'>Save</button>
-                                </div>
-                            </div>
-                        </div>
+                <form>
+                    <div className='car-plate-center'>
+                        <label className='inner-cms-center'>
+                            License Plate
+                        <input type='text' value={this.state.licensePlate} onChange={this.handleChange} name='licensePlate' />
+                        </label>
                     </div>
-                </div>
+                    <div className='car-brands-center'> 
+                        <label className='inner-cms-center'>
+                            Brands
+                        <input type='text' value={this.state.carBrands} onChange={this.handleChange} name='carBrands'/>    
+                        </label>
+                    </div>
+                    <div className='car-models-center'> 
+                        <label className='inner-cms-center'>
+                            Models
+                        <input type='text' value={this.state.carModels} onChange={this.handleChange} name='carModels'/>
+                        </label>
+                    </div>
+                    <div className='car-remarks-center'>
+                        <label className='inner-cms-center'>
+                            Remark
+                        <input type='text' value={this.state.carRemarks} onChange={this.handleChange} name='carRemarks'/>
+                        </label>
+                    </div>
+                    <div className='cms-button'>
+                        <button type='button' className='cms-save-button'>Save</button>
+                    </div>
+                </form>
             )
         }
     }
